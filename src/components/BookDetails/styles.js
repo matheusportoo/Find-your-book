1d507fb3802d5602a96b266276c73ff91a9017c8
Matheus Porto <matheusportoo@gmail.com>
2021-03-27 10:38:08 -0300

    add: create components and style for book detail page

---
+++ src/components/BookDetails/styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { mediaQuery } from '../../tools';
+
+export const BookDetails = {};
+
+BookDetails.Title = styled.h2`
+  margin-top: 0;
+  margin-bottom: 0;
+  line-height: 1.3;
+  font-size: 4rem;
+
+  ${mediaQuery.tabletDesktop`
+    font-size: 6.6rem;
+  `}
+`;
+
+BookDetails.Subtitle = styled.h3`
+  font-size: 2.6rem;
+  font-weight: var(--font-weight-light);
+  margin-bottom: 40px;
+`;
+
+BookDetails.Info = styled.dl`
+  font-size: 1.6rem;
+`;
+
+BookDetails.InfoKey = styled.dt`
+  font-weight: var(--font-weight-black);
+  margin-top: 20px;
+`;
+BookDetails.InfoValue = styled.dd`
+  font-weight: var(--font-weight-regular);
+  margin-left: 0;
+`;
+
+BookDetails.Author = styled.p`
+  font-size: 1.6rem;
+`;
+
+BookDetails.Categories = styled.p`
+  display: inline-block;
+  background-color: var(--color-highlight);
+  padding: 8px;
+  border-radius: 2px;
+  font-weight: var(--font-weight-black);
+  font-size: 1.4rem;
+`;
--- src/pages/BookDetail/index.js
+++ src/pages/BookDetail/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-const BookDetail = () => <h1>Book detail page</h1>;
+import Header from '../../components/Header';
+import Content from '../../components/Content';
+import BookDetails from '../../components/BookDetails';
+
+import response from '../../data/response.json';
+
+const BookDetail = () => {
+  const book = response.items[8];
+
+  return (
+    <>
+      <Header title="Encontre seu livro" />
+      <Content>
+        <BookDetails
+          book={{
+            title: book.volumeInfo.title,
+            subtitle: book.volumeInfo.subtitle,
+            authors: book.volumeInfo.authors,
+            year: book.volumeInfo.publishedDate,
+            totalPages: book.volumeInfo.pageCount,
+            description: book.volumeInfo.description,
+            categories: book.volumeInfo.categories
+          }}
+        />
+      </Content>
+    </>
+  );
+};
 
 export default BookDetail;
